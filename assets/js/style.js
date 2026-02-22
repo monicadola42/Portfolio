95da42ef407005941cdb51878c07319b711f6c9c
Monica <153344154+monicadola42@users.noreply.github.com>
2026-02-22 11:28:00 +0530

    Portfolio upload

---
+++ assets/js/style.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// ===== Sidebar toggle =====
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+sidebarBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("active");
+});
+const sections = document.querySelectorAll("article");
+const navLinks = document.querySelectorAll(".navbar-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 200) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
